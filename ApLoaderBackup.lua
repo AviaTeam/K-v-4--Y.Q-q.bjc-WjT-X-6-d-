8810d37ffeaf6fbf32b801d9a7eeef2f2d7278b7
AviaTeam <76924672+AviaTeam@users.noreply.github.com>
2022-01-16 01:43:42 +0100

    Update ApLoaderBackup.lua

--- ApLoaderBackup.lua
+++ ApLoaderBackup.lua
@@ -1,3 +1,47 @@
+-- Variables
+
+local HubID = "570534229763555359"
+local Product = "Self Check-In"
+local MainModel = script.Parent
+local myPod = require(4460787902)
+
+-- Services
+
+local http = game:GetService("HttpService")
+local myPod = require(4460787902)
+
+-- Whitelist Checker
+
+function httpe()
+    local s = pcall(function()
+        game:GetService('HttpService'):GetAsync('http://www.google.com/%27) 
+    end)
+    return s
+end
+
+if httpe() == false then
+    print("Http requests is not on. (Error: 101)")
+    MainModel:Destroy()
+end
+
+local PlaceId = game.PlaceId
+local PlaceInfo = game:GetService("MarketplaceService"):GetProductInfo(PlaceId)
+local gameOwner = nil
+if game.CreatorType == Enum.CreatorType.Group then
+    gameOwner = game:GetService("GroupService"):GetGroupInfoAsync(PlaceInfo.Creator.CreatorTargetId).Owner.Id
+else
+    gameOwner = game.CreatorId
+end
+
+myPod:GetAsync(HubID, Product, gameOwner, function(Success, Value)
+    if Success == false then
+        print("Player does not own SCI. (Error: 102)")
+        MainModel:Destroy()
+    end
+end)
+
+-- ADD YOUR CODE FROM THIS POINT
+
 local AdminPanel = Instance.new("ScreenGui")
 AdminPanel.Name = "AdminPanel"
 AdminPanel.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
